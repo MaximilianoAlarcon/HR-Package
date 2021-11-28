42a92b00caa61fcd0b38ea36320d8c51d9722940
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-28 02:39:25 -0300

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers2.py
+++ my_custom_sklearn_transforms/sklearn_transformers2.py
@@ -2,10 +2,9 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import tensorflow as tf
 
 class Poly_data(BaseEstimator, TransformerMixin):
-    def __init__(self, polynomialfeatures_transformer, standardscaler_poly_data, model):
+    def __init__(self, polynomialfeatures_transformer, standardscaler_poly_data):
         self.polynomialfeatures_transformer = polynomialfeatures_transformer
         self.standardscaler_poly_data = standardscaler_poly_data
-        self.model = model
     
     def fit(self, X, y=None):
         return self
@@ -14,6 +13,6 @@ class Poly_data(BaseEstimator, TransformerMixin):
         data = X.copy()
         data = self.polynomialfeatures_transformer.transform(data)
         data = self.standardscaler_poly_data.transform(data)
-        #model = tf.keras.models.load_model('./deep_learning_model.h5py')
-        data = self.model.predict(data)
+        model = tf.keras.models.load_model('./deep_learning_model.h5py')
+        data = model.predict(data)
         return data
