6810a81bc991154e3808ea275384ba2e84147b90
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-12-01 15:35:55 -0300

    Update sklearn_transformers3.py

--- my_custom_sklearn_transforms/sklearn_transformers3.py
+++ my_custom_sklearn_transforms/sklearn_transformers3.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, RegressorMixin
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
 
 class Model(BaseEstimator, RegressorMixin):
     def __init__(self, ruta_modelo):
@@ -10,6 +12,11 @@ class Model(BaseEstimator, RegressorMixin):
 
     def predict(self, X):      
         data = X.copy()
-        model = tf.keras.models.load_model(self.ruta_modelo)
+        #model = tf.keras.models.load_model(self.ruta_modelo)
+        model = Sequential()
+        model.add(Dense(32,kernel_initializer='normal',input_dim = 70,activation="relu"))
+        model.add(Dense(1,activation = "linear")) 
+        model.compile(optimizer = "adam",loss="mse",metrics=['mse'])
+        model.load_weights('model_savedfile_format/model_nn_reg')
         data = model.predict(data)
         return data
